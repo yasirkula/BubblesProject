edd11f007d109a78490f9167c4f64070c2200300
srazynp <esrazeynepk@gmail.com>
2014-12-21 21:10:35 +0200

    Update SettingsManager.java

--- src/Database/SettingsManager.java
+++ src/Database/SettingsManager.java
@@ -1,10 +1,3 @@
-/**
- * SettingsManager - A database that fetches stored settings from local disk
- * 					 and writes them back when necessary
- * 
- * @author CS319 - Section 2 - Group 9
- */
-
 package Database;
 
 import java.awt.Color;
@@ -21,41 +14,42 @@ public class SettingsManager
 	// VARIABLES
 	private float soundLevel;
 	private Color backgroundColor;
+	public int lockedLevelNumberForBio, lockedLevelNumberForChem, lockedLevelNumberForVocab;
 	// END OF VARIABLES
-	
+
 	// CONSTRUCTORS
 	public SettingsManager()
 	{
 		// load default values and then try to fetch values from database
 		soundLevel = 1f;
 		backgroundColor = Color.white;
-		
+
 		loadSettings();
 	}
 	// END OF CONSTRUCTORS
-	
+
 	// MUTATOR - ACCESSOR METHODS
 	public float getSoundLevel()
 	{
 		return soundLevel;
 	}
-	
+
 	public void setSoundLevel( float s )
 	{
 		soundLevel = s;
 	}
-	
+
 	public Color getBackgroundColor()
 	{
 		return backgroundColor;
 	}
-	
+
 	public void setBackgroundColor( Color c )
 	{
 		backgroundColor = c;
 	}
 	// END OF MUTATOR - ACCESSOR METHODS
-	
+
 	// OTHER METHODS
 	public void loadSettings()
 	{
@@ -65,80 +59,96 @@ public class SettingsManager
 		// -text-file-with-utf-8-encoding-using-java
 		// answered by: Shobit Sharma
 		BufferedReader buffer = null;
-		
+
 		try
 		{
 			buffer = new BufferedReader( new InputStreamReader( 
 					new FileInputStream( "settings.dat" ), "UTF-8" ) );
-		    
+
 			String line = null;
-		    while( ( line = buffer.readLine() ) != null ) 
-		    {
-		        String[] tokens = line.split( " " );
-		        
-		        if( tokens[0].equals( "Sound" ) )
-		        {
-		        	// try to fetch sound level setting
-		        	try
-		        	{
-		        		soundLevel = Integer.parseInt( tokens[1] ) / 100f;
-		        	}
-		        	catch( Exception e ){}
-		        }
-		        else if( tokens[0].equals( "Bg" ) )
-		        {
-		        	// try to fetch background color setting
-		        	try
-		        	{
-		        		for( int i = 0; i < tokens.length; i++ )
-				        {
-				        	int r = Integer.parseInt( tokens[1] );
-				        	int g = Integer.parseInt( tokens[2] );
-				        	int b = Integer.parseInt( tokens[3] );
-				        	
-				        	backgroundColor = new Color( r, g, b );
-				        }
-		        	}
-		        	catch( Exception e ){}
-		        }
-		    }
+			while( ( line = buffer.readLine() ) != null ) 
+			{
+				String[] tokens = line.split( " " );
+
+				if( tokens[0].equals( "Sound" ) )
+				{
+					try
+					{
+						soundLevel = Integer.parseInt( tokens[1] ) / 100f;
+					}
+					catch( Exception e ){}
+				}
+				else if( tokens[0].equals( "Bg" ) )
+				{
+					try
+					{
+						for( int i = 0; i < tokens.length; i++ )
+						{
+							int r = Integer.parseInt( tokens[1] );
+							int g = Integer.parseInt( tokens[2] );
+							int b = Integer.parseInt( tokens[3] );
+
+							backgroundColor = new Color( r, g, b );
+						}
+					}
+					catch( Exception e ){}
+				}
+				else if( tokens[0].equals( "LockedLevel" ) )
+				{
+					try
+					{
+						for( int i = 0; i < tokens.length; i++ )
+						{
+							lockedLevelNumberForBio= Integer.parseInt( tokens[1] );
+							lockedLevelNumberForChem= Integer.parseInt( tokens[2] );
+							lockedLevelNumberForVocab = Integer.parseInt( tokens[3] );
+
+						}
+					}
+					catch( Exception e ){}
+				}
+			}
 		} 
 		catch( IOException x ){}
 		finally 
 		{
-		   try 
-		   {
-			   buffer.close();
-		   } 
-		   catch( Exception ex ){}
+			try 
+			{
+				buffer.close();
+			} 
+			catch( Exception ex ){}
 		}
 	}
-	
+
 	public void writeSettings()
 	{
 		// Inspired from http://stackoverflow.com/questions/2885173/java
 		// 				 -how-to-create-and-write-to-a-file
 		// answered by: Bozho
-		
+
 		BufferedWriter buffer = null;
 
 		try 
 		{
-		    buffer = new BufferedWriter( new OutputStreamWriter(
-		          new FileOutputStream( "settings.dat" ), "UTF-8" ) );
-		    buffer.write( "Sound " + (int)( soundLevel * 100 ) );
-		    buffer.newLine();
-		    buffer.write( "Bg " +  backgroundColor.getRed() + " " +
-		    					backgroundColor.getGreen() + " " + backgroundColor.getBlue() );
+			buffer = new BufferedWriter( new OutputStreamWriter(
+					new FileOutputStream( "settings.dat" ), "UTF-8" ) );
+			buffer.write( "Sound " + (int)( soundLevel * 100 ) );
+			buffer.newLine();
+			buffer.write( "Bg " +  backgroundColor.getRed() + " " +
+					backgroundColor.getGreen() + " " + backgroundColor.getBlue() );
+			buffer.newLine();
+			buffer.write( "LockedLevel " +  lockedLevelNumberForBio + " " +
+					lockedLevelNumberForChem + " " + lockedLevelNumberForVocab );
+
 		} 
 		catch( IOException ex ){} 
 		finally 
 		{
-		   try 
-		   {
-			   buffer.close();
-		   } 
-		   catch( Exception ex ){}
+			try 
+			{
+				buffer.close();
+			} 
+			catch( Exception ex ){}
 		}
 	}
 	// END OF OTHER METHODS
